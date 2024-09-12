bd6c480e828dc8186209a8400ad00d87c858a304
Ayyy13579 <zaidzubairulhassan11@gmail.com>
2024-09-12 22:37:15 +0500

    Add files via upload

---
+++ Lab3/Question3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+    float taxrate, salary;
+    printf("Enter current tax rate: ");
+    scanf("%f",&taxrate);
+    printf("Enter current salary: ");
+    scanf("%f",&salary);
+    salary = salary * (1 - taxrate/100);
+    printf("New salary is %f", salary);
+
+}
---
+++ Lab3/Question5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+    int value, rate, timeperiod;
+    float interest;
+    printf ("Enter a value between 100 and 1,000,000: ");
+    scanf ("%d",&value);
+    printf ("Enter a rate of interest between 5 and 10: ");
+    scanf ("%d",&rate);
+    printf ("Enter a time period between 1 and 10 years: ");
+    scanf ("%d",&timeperiod);
+    interest = (timeperiod*rate*value)/100;
+    printf ("The interest is: %f\n",interest);
+
+
+}
