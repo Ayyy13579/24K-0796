a5b491366debf4391e707c5c1276255346371567
Ayyy13579 <zaidzubairulhassan@gmail.com>
2024-11-21 23:03:33 +0500

    compare functions
    Co-authored-by: Adnan <adnanosama@users.noreply.github.com>

--- PF_Project/hangman.c
+++ PF_Project/hangman.c
@@ -3,23 +3,46 @@
 #include <math.h>
 #include <stdlib.h>
 
-void encryptword(char word[20]);
-void finaldecrypt(char word[20]);
-void encryptword(char word[20]) {
-    char encryptedword[20] = {};
+char encryptword(char word[20], char encryptedword[20]);
+// void finaldecrypt(char word[20]);
+
+char encryptword(char word[20], char encryptedword[20]) {
     int length = strlen(word);
+    int randomindex[length];
+    int countrandomindex=0;
+    for (int i = 0; i < length; i++) {
+        randomindex[i]=countrandomindex;
+        countrandomindex++;
+    }
+    
     for (int i = 0; i < length; i++) {
         encryptedword[i] = "_";
     }
-    
-    if (length < 5) {
+    if (length <= 5) {
         int hint = rand() % length;
+        randomindex[hint]=-1;
         encryptedword[hint] = word[hint];
+    } else if (length <= 8) {
+        for (int i = 0; i < 2; i++){
+            int hint = rand() % length;
+            randomindex[hint]=-1; 
+        }
+    } else {
+        for (int i = 0; i < 3; i++){
+            int hint = rand() % length;
+            encryptedword[hint] = word[hint];
+        }
+    }
+    return encryptedword;
 }
-void finaldecrypt(char word[20]){
-    printf("You lost.\n");
-    printf("the word was %s", word);
-}
+
+
+// void finaldecrypt(char word[20]){
+//     printf("You lost.\n");
+//     printf("the word was %s", word);
+// }
+
+
 
 char randomline(char filename[]) {
     FILE *file;
@@ -32,14 +55,35 @@ char randomline(char filename[]) {
         count++;
        
     }    
-} 
+}
+void displayEncrypted() {
+}
+
+char compareandinput(char userchoice,int tries, char word[20], char encryptedword[20]) {
+    int charindex = 0;
+    char found = 'F';
+    for (int i=0;i<strlen(word); i++) {
+        if (userchoice == word[i] || (userchoice + 32) == word[i]) {
+            found = 'T';
+            int charindex = i;
+            break;
+        }
+    }
+    if (found == 'T') {
+        encryptedword[charindex] = word[charindex];
+        fputs(encryptedword,stdout);
+    }
+    return found;
+
+}
+
 
 void main() {
     int choice;
     while (1) {
     printf("Welcome to Hangman!\n");
     printf("Please choose a category to generate your word.\n");
-    printf("1. Transport\n2. Fruits and Vegetables\n3. Countries\n4. Sport\n5. Animals\n6. Exit Program");
+    printf("1. Transport\n2. Fruits and Vegetables\n3. Countries\n4. Sport\n5. Animals\n6. Exit Program\n");
     printf("Choose your category: ");
     scanf("%d", &choice);
     switch (choice) {
@@ -47,6 +91,7 @@ void main() {
         char filename[] = "transport.txt";
         char word[20] = randomline(filename);
         encryptword(word);
+        printf("Enter an alphabet ");
         break;
     case 2:
         char filename[] = "fruits.txt";
@@ -77,31 +122,3 @@ void main() {
     }
 }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
