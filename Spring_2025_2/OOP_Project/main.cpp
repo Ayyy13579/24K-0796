57a2f88f69528b448231f5a937aab69cd682ea9f
Ayyy13579 <zaidzubairulhassan@gmail.com>
2025-05-13 12:58:12 +0500

    complete wave mechanics and alert boxes

--- Spring_2025_2/OOP_Project/main.cpp
+++ Spring_2025_2/OOP_Project/main.cpp
@@ -2,11 +2,11 @@
 #include <SFML/Audio.hpp>
 #include "map.h"
 #include "zombie.h"
+#include "wave.h"
 #include <iostream>
 #include <vector>
 
-int main()
-{
+int main() {
     sf::RenderWindow window(sf::VideoMode(1000, 900), "Tower Game");
     window.setFramerateLimit(60);
 
@@ -14,7 +14,9 @@ int main()
     sf::Texture towerTexture;
     sf::Texture pathTexture;
     sf::Texture chestTexture;
-    sf::Texture zombieTexture;
+    sf::Texture regzombieTexture;
+    sf::Texture fastzombieTexture;
+    sf::Texture strzombieTexture;
 
     const int tileSize = Map::tileSize;
     std::vector<sf::Vector2f> path = {
@@ -28,48 +30,46 @@ int main()
         {5 * tileSize, 7 * tileSize},
         {5 * tileSize, 8 * tileSize},
     };
+
     if (!grassTexture.loadFromFile("images/grass.jpeg") ||
         !towerTexture.loadFromFile("images/tower.png") ||
-        !pathTexture.loadFromFile("images/path.jpg") || 
+        !pathTexture.loadFromFile("images/path.jpg") ||
         !chestTexture.loadFromFile("images/chest.jpg") ||
-        !zombieTexture.loadFromFile("images/zombie.png")) {
-        throw std::runtime_error("failed to load textures");
+        !regzombieTexture.loadFromFile("images/zombie.png") ||
+        !fastzombieTexture.loadFromFile("images/fastzombie.png") ||
+        !strzombieTexture.loadFromFile("images/strongzombie.png")) {
+        std::cerr << "Error loading textures" << std::endl;
     }
 
-    std::vector<Zombie> zombies;
-    FastZombie fastZombie(zombieTexture, path);
-    StrongZombie strongZombie(zombieTexture, path);
+    Waves waves(regzombieTexture, fastzombieTexture, strzombieTexture, path);
     sf::Clock clock;
     float spawnTimer = 0.0f;
     int zombiesSpawned = 0;
     const int maxZombies = 5;
-    
-    while (window.isOpen())
-    {
+    bool damage = false;
+
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::E) {
+                    damage = true;
+                }
+            }
         }
         window.clear();
         Map::drawMap(window, grassTexture, towerTexture, pathTexture, chestTexture);
         float deltaTime = clock.restart().asSeconds();
-        spawnTimer += deltaTime;
-        if (spawnTimer >= 1.0f && zombiesSpawned < maxZombies) {
-            zombies.emplace_back(zombieTexture, path);
-            zombiesSpawned++;
-            spawnTimer = 0.0f;
+        waves.update(deltaTime);
+        waves.drawZombies(window);
+        if (damage) {
+            for (Zombie* z : waves.getZombies()) {
+                z->takeDamage(1);
             }
-        for (Zombie& z : zombies) {
-            z.update(deltaTime);
-            z.draw(window);
-            }
-        fastZombie.update(deltaTime);
-        fastZombie.draw(window);
-        strongZombie.update(deltaTime);
-        strongZombie.draw(window);
+            damage = false;
+        }
         window.display();
     }
-    return 0;
 }
